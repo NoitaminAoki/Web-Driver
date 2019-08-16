64f01fc9f16228fc0cb79ff4ea5d6981a59a8140
adityarifai <aditya.rifai07@gmai.com>
2019-08-16 16:05:16 +0700

    [PRE-FINISH] all of

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,7 +7,6 @@ use Illuminate\Http\Request;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use App\User;
 use Auth;
-use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -45,11 +44,7 @@ class LoginController extends Controller
     {
         $user = User::where('no_pol', '=', $request->no_pol)->first();
         if(empty($user)) {
-<<<<<<< HEAD
             $error = 'Nomor Polisi atau password yang anda masukkan salah! | Pastikan Nomor Polisi yang anda masukkan tidak memakai spasi/jarak.';
-=======
-            $error = ['error' => 'Nomor Polisi atau password yang anda masukkan salah!'];
->>>>>>> e351f4ef39b96e1f2fba4070859ecb010bc64bf6
             return redirect()->back()->withErrors($error);
         }
         Auth::login($user);
